3389edc653a7724936238101e00c7099380e531d
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-09-29 11:16:49 -0700

    commit tree ff2fd91cb90d3ff0c627bf120ee8369eaf39c909

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line queue
+  if queue.length == 0
+    puts "The line is currently empty."
+  else
+    str = "The line is currently:"
+    queue.each_with_index do |customer, index|
+      str << " #{index + 1}. #{customer}"
+    end
+    puts str
+  end   
+end
+
+def take_a_number (queue, customer)
+  queue << customer
+  puts "Welcome, #{customer}. You are number #{queue.length} in line."
+end
+
+def now_serving queue
+  if queue.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    next_customer = queue.shift
+    puts "Currently serving #{next_customer}."
+  end
+end
